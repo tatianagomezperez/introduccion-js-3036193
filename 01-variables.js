1984a82147d6232e6b1600de236dab00288e5592
Tatiana Gomez <yeimitatianagomezperez@gmail.com>
2025-08-19 16:03:54 -0500

    declaracion de variables y tipo de dato string

---
+++ 01-variables.js
@@ -0,0 +1,54 @@
+// alert("Hello world ")
+// =========================================
+// CONVENCION NOMBRES PARA CODIFICAR
+// =========================================
+
+// UpperCamelCase => en js se usa para nombrar las clases 
+// lowerCamelCase => en js se usa para nombrar las variables
+// SCREAMING_SNAKE_CASE => se usa para nombrar las constantes 
+
+
+// snake_case     => en js generalmente no se usa, esta convencion es muy usada en el lenguaje python
+// caja-de-kebab     => se usa para nombrar las imagenes, los repositorios
+// caja-de-tren      =>Ejemplo-De-Proyecto 
+
+
+// variables
+// ya no se declara un variable con var
+// en js no es obligatorio terminar una sentencia con ;
+// en js no es obligatorio indicar el tipo de variable
+
+var product = "sensor-1";
+
+// // no se puede inicar una variable con - al medio
+// var -sensor = "sensor-2";
+
+// // no se puede iniciar una variable con numero
+// var 1sensor = "sensor-3";
+
+// si es permitido iniciar una variable con guion al piso _
+var _sensor = "sensor-4";
+
+// crear una variable con las tres formas
+
+var sensor = "Sensor de lluvia";
+var sensor = "Sensor de temperatura";
+var sensor = "Sensor de presion";
+
+console.log(sensor);
+
+
+let userName = "Tatiana";
+// let userName = "juan";
+console.log(userName); 
+
+// ejemplo con const
+// no puedo cambiarle el valor a una variable constante
+const maxSize = 1024;
+maxSize = 124568;
+
+
+// otro ejemplo con var
+console.log(userName2)
+// var userName2 = "Jacobo";
+let userName2 = "Jacobo";
---
+++ 02-tipo-dato-string.js
@@ -0,0 +1,19 @@
+// string o cadena de texto
+const sensor = "temperatura";
+const sensor2 = "ph";
+
+// dos formas no tan comunes
+const sensor3 = String('presion');
+const sensor4 = new String('presion');
+
+console.log(sensor);
+console.log(sensor3)
+
+// con typeof se puede ver el tipo de dato
+console.log(typeof sensor2);
+console.log(sensor2);
+
+let product  = "Televisor de 40\"";
+let product2 = "Televisor de 32\"";
+console.log(product); 
+console.log(product2); 
